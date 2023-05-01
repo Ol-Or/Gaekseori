f71c2fb301dcc3a4b884c1ced412306f0159812b
Ol-Or <suna42378@gmail.com>
2023-05-01 20:25:07 +0900

    Update servo.py

--- servo.py
+++ servo.py
@@ -36,14 +36,22 @@ def setServoPos(degree):
     servo.ChangeDutyCycle(duty)
 
 
-if __name__ == "__main__":
-    # 서보 0도에 위치
-    setServoPos(0)
+if humidity >= 알수 없는 값:  # 여기 빗물이 있을 때 습도 값을 몰라서 아직 작성안함
+    setServoPos(0)    # 서보 0도에 위치
     sleep(1)  # 1초 대기
 
     # 180도에 위치
     setServoPos(180)
     sleep(1)
+
+    # 서보 PWM 정지
+    servo.stop()
+    # GPIO 모드 초기화
+    GPIO.cleanup()
+
+else:
+    setServoPos(0)    # 서보 0도에 위치
+    sleep(1)  # 1초 대기
 
     # 서보 PWM 정지
     servo.stop()
