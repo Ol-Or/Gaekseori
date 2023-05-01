889eb428c667109113fa0258efb838c68cba9cd1
LeeMiRyeong <117430814+ddddddddggod@users.noreply.github.com>
2023-05-01 23:23:43 +0900

    Add files via upload

---
+++ test_water level.py
@@ -0,0 +1,17 @@
+import smbus
+import time
+
+address = 0x48
+AIN2 = 0x42
+
+bus=smbus.SMBus(1)
+
+try:
+    while True:
+        bus.write_byte(adress,AIN2)
+        value = bus.read_byte(address)
+        print("Water value: %d" % value)
+        time.sleep(0.1)
+
+except KeyboardInterrupt:
+    pass
