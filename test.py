769135d14ba160504c4308c98f08c4bf14805cc7
Ol-Or <suna42378@gmail.com>
2023-05-04 21:52:32 +0900

    Update test.py

--- test.py
+++ test.py
@@ -20,7 +20,7 @@ while True:
 #!!!!!!!!!!!!!!! 2 온습도센서 test!!!!!!!!!!!!!!!!!!
 
 sensor = Adafruit_DHT.DHT11  # 습도 센서 모델 선택
-pin = 4  # 습도 센서 신호선을 연결한 GPIO 핀
+pin = 21  # 습도 센서 신호선을 연결한 GPIO 핀
 
 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
