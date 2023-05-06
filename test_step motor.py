7fa69ddc5001306fe49e21a460d08adb89dbe5bc
LeeMiRyeong <117430814+ddddddddggod@users.noreply.github.com>
2023-05-06 17:13:24 +0900

    Update test_step motor.py

--- test_step motor.py
+++ test_step motor.py
@@ -2,60 +2,50 @@ import time
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
-StepPins=[11,13,15,16]
+StepPins = [11, 13, 15, 16]
 
 for pin in StepPins:
-    GPIO.stepup(pin,GPIO.OUT)
-    GPIO.output(pin,False)
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, False)
 
 StepCounter = 0
+StepCount = 4
 
-StepCount=4
-
-
-for pin in range(0,4):
-    Seq = [[0, 0, 0, 1],  #정방향
-            [0, 0, 1, 0],
-            [0, 1, 0, 0],
-            [1, 0, 0, 0]]
-    xpin = StepPins[pin]
-    if Seq[StepCounter][pin] !=0:
-        GPIO.output(xpin, True)
-    else:
-        GPIO.output(xpin,False)
- 
+for i in range(5):
+    for pin in range(0, 4):
+        Seq = [[1, 0, 0, 1],  # 정방향
+               [1, 0, 1, 0],
+               [0, 1, 1, 0],
+               [0, 1, 0, 1]]
+        xpin = StepPins[pin]
+        if Seq[StepCounter][pin] != 0:
+            GPIO.output(xpin, True)
+        else:
+            GPIO.output(xpin, False)
         StepCounter += 1
-
-        if (StepCounter ==StepCount):
-            StepCounter =0
-        if (StepCounter<0):
+        if StepCounter == StepCount:
+            StepCounter = 0
+        elif StepCounter < 0:
             StepCounter = StepCount
+    time.sleep(0.002)
 
-        time.sleep(0.01)
-        
-    time.sleep(5)  #5초간 대기
-
-for pin in range(0,4):
-    Seq = [[0, 0, 0, 1],    #역방향
-            [0, 0, 1, 0],
-            [0, 1, 0, 0],
-            [1, 0, 0, 0]]
-    xpin = StepPins[pin]
-    if Seq[StepCounter][pin] !=0:
-        GPIO.output(xpin, True)
-    else:
-        GPIO.output(xpin,False)
- 
+for i in range(5):
+    for pin in range(3, -1, -1):
+        Seq = [[1, 0, 0, 1],  # 역방향
+               [1, 0, 1, 0],
+               [0, 1, 1, 0],
+               [0, 1, 0, 1]]
+        xpin = StepPins[pin]
+        if Seq[StepCounter][pin] != 0:
+            GPIO.output(xpin, True)
+        else:
+            GPIO.output(xpin, False)
         StepCounter += 1
-
-        if (StepCounter ==StepCount):
-            StepCounter =0
-        if (StepCounter<0):
+        if StepCounter == StepCount:
+            StepCounter = 0
+        elif StepCounter < 0:
             StepCounter = StepCount
+    time.sleep(0.002)
 
-        time.sleep(0.01)  
+GPIO.cleanup()
 
-
-
-except KeyboardInterrupt:
-    GPIO.cleanup()
