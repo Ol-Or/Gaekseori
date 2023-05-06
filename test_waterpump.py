21694463e634b9daee5c7ddfcf9f444b2905b56c
LeeMiRyeong <117430814+ddddddddggod@users.noreply.github.com>
2023-05-06 18:09:25 +0900

    Update test_waterpump.py

--- test_waterpump.py
+++ test_waterpump.py
@@ -1,25 +1,16 @@
 import RPi.GPIO as GPIO
 import time
 
-pump_channel = 23
+A1A = 23
+GPIO.setup(A1A, GPIO.OUT)
+GPIO.output(A1A, GPIO.LOW)
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(pump_channel, GPIO.OUT)
-
-# 워터펌프를 작동시키는 함수
-def turn_on_pump():
-    GPIO.output(PUMP_PIN, GPIO.HIGH)
-    print("turning on the water pump")
-
-# 워터펌프를 중지시키는 함수
-
-
-def turn_off_pump():
-    GPIO.output(PUMP_PIN, GPIO.LOW)
-    print("turning off the water pump")
 
 while True:
-    turn_on_pump()  # 워터펌프 on
-    time.sleep(5)  # 워터펌프 5초동안 작동
-    turn_off_pump()  # 워터펌프 off
+    GPIO.output(A1A, GPIO.HIGH)
+    time.sleep(5)
+    GPIO.output(A1A, GPIO.LOW)
+
+GPIO.cleanup
 
