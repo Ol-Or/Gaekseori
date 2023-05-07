dfe0de0cddc6cabbcb09b880003f77193c6aac4b
Ol-Or <suna42378@gmail.com>
2023-05-07 20:39:04 +0900

    final_flask Update

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,9 @@
 import RPi.GPIO as GPIO
 import time
 from time import sleep
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 app = Flask(__name__)
+
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -16,74 +17,32 @@ IN2 = 13  # Input Pin
 ENA = 15  # Enable Pin
 # 워터펌프 지붕
 A1A = 23
-
-# 스텝모터 & 워터펌프 셋업
-GPIO.setup(IN1, GPIO.OUT)
-GPIO.setup(IN2, GPIO.OUT)
-GPIO.setup(ENA, GPIO.OUT)
-GPIO.setup(A1A, GPIO.OUT)
-
-
-# 스텝모터 & 워터펌프 초기화
-
-GPIO.output(IN1, GPIO.LOW)
-GPIO.output(IN2, GPIO.LOW)
-GPIO.output(ENA, GPIO.LOW)
-GPIO.output(A1A, GPIO.LOW)
+# LED 핀들을 딕셔너리 변수로 선언
+pin_dict = {'IN1': 11, 'IN2': 13, 'ENA': 15, 'A1A': 23}
+GPIO.setup(pin_dict['IN1'], GPIO.OUT)   # 각각의 LED 핀들을 출력으로 설정
+GPIO.setup(pin_dict['IN2'], GPIO.OUT)
+GPIO.setup(pin_dict['ENA'], GPIO.OUT)   # 각각 LED의 현재 상태를 나타내는 변수
+GPIO.setup(pin_dict['A1A'], GPIO.OUT)   # 각각 LED의 현재 상태를 나타내는 변수
+state_dict = {'IN1': 0, 'IN2': 0, 'ENA': 0, 'A1A': 0}
 
 
-# def
+@app.route('/')                     # 기본 주소
+def hello():                         # 여기서 index.html을 화면에 보여주며, LED 현황을 전달
+    return render_template('main.html', state_dict=state_dict)
+
+
+@app.route('/<pin>/<int:state>')  # 각각의 pin을 켜고 끄기 위한 주소
+def control(pin, state):      # 각각의 pin을 켜고 끄기 위한 뷰함수
+    state_dict[pin] = state   # pin과 state를 전달받아 현황을 갱신함
+    # 참고: color와 state를 전달받는 부분은 index.html 부분에 코드가 있음
+    GPIO.output(pin_dict['IN1'], state_dict['IN1'])
+    GPIO.output(pin_dict['IN2'], state_dict['IN2'])
+    GPIO.output(pin_dict['A1A'], state_dict['A1A'])
+    return redirect(url_for('hello'))    # 제어가 끝나면 기본주소로 돌아감
+
+
 # 스텝모터 차수판
 
-# 모터 앞으로 이동
-def motorOn():
-	print("forward motion")
-	GPIO.output(IN1, True)  # forward
-    GPIO.output(IN2, False)
-    time.sleep(5)     #시간 측정 해보고 설정할거임
-#모터 뒤로 이동
-def motorOff():
-	print ("backward motion")
-	GPIO.output(IN1, GPIO.LOW)
-	GPIO.output(IN2, GPIO.HIGH)
-	time.sleep(5)  #시간 측정 해보고 설정할거임
-#모터 정지
-
-
-#워터펌프 동작
-def waterPumpOn(duration):
-    GPIO.output(A1A, GPIO.HIGH)
-    time.sleep(duration)
-    GPIO.output(A1A, GPIO.LOW)
-
-def waterPumpOn():
-	GPIO.output(A1A, GPIO.LOW)
-
-@app.route('/')
-def hello():
-	return render_template('main.html')
-
-#차수판의 스텝모터 동작
-@app.route('/motoron')
-def motor_on():
-	motorOn()
-	return ('OK'), 204
-
-@app.route('/motoroff')
-def motor_off():
-	motorOff()
-	return ('OK'), 204
-
-
-@app.route('/pumpon')
-def pump_on():
-	waterPumpOn()
-	return ('OK'), 204
-
-@app.route('/pumpoff')
-def pump_off():
-	waterPumpOn()
-	return ('OK'), 204
 
 if __name__ == "__main__":
-	app.run(debug=True, host='0.0.0.0')
+    app.run(host="192.168.207.211", port="9090")
