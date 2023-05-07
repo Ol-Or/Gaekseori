d2f7eb9c4ac1521fade63b2e1a4f58820af78b7b
LeeMiRyeong <117430814+ddddddddggod@users.noreply.github.com>
2023-05-08 00:31:03 +0900

    Add files via upload

---
+++ tlqkf.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+import time
+import Adafruit_DHT
+import smbus
+
+sensor = Adafruit_DHT.DHT11
+pump2_channel =20      #heatwave
+ht_pin = 21
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pump1_channel, GPIO.OUT)
+GPIO.output(pump1_channel, GPIO.LOW)
+
+humidity,temperature= Adafruit_DHT.read_retry(sensor,ht_pin)
+
+while True:
+        if temperature >= 30: 
+            print('temperature = {0:0.1f}*C ,water pump on!',format(temperature))
+            GPIO.output(pump2_channel, GPIO.HIGH)
+            time.sleep(7)
+            GPIO.output(pump2_channel, GPIO.LOW)
+            
+        else:
+            GPIO.output(pump2_channel, GPIO.LOW)
+        time.sleep(1)
