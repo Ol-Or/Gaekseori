f2284a8ca61f6064b5318cda8d6383281ef16cf0
LeeMiRyeong <117430814+ddddddddggod@users.noreply.github.com>
2023-05-08 01:22:49 +0900

    Update step motor and water level.py

--- step motor and water level.py
+++ step motor and water level.py
@@ -6,12 +6,6 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 StepPins=[11,13,15,16]  #step motor GPIO
 
-#PCF module address
-address = 0x48
-AIN2 = 0x42
-
-bus=smbus.SMBus(1)
-
 for pin in StepPins:
     GPIO.setup(pin,GPIO.OUT)
     GPIO.output(pin,False)
@@ -30,38 +24,40 @@ SeqCounterClockwise = [  #반시계방향
 ]
 
 direction = True
+Seq= SeqClockwise
+
+#PCF module address
+address = 0x48
+AIN2 = 0x42
+
+bus=smbus.SMBus(1)
 
 StepCounter = 0
 
 StepCount=4
 
-try:
-   while True:
-        bus.write_byte(address,AIN2)
-        value = bus.read_byte(address)
-        if value > 256: # water level
-            print('Flood is occured!')
-            Seq = SeqClockwise if direction else SeqCounterClockwise
-        else :
-            Seq = SeqCounterClockwise if direction else SeqClockwise   #시계방향(앱으로 구현)
-            print('good!')
+while True:
+    bus.write_byte(address,AIN2)
+    value = bus.read_byte(address)
 
-        for pin in range(0, 4):
-            xpin = StepPins[pin]
-            if Seq[StepCounter][pin] != 0:
-                GPIO.output(xpin, True)
-            else:
-                GPIO.output(xpin, False)
+    if value > 256: # 수위 측정한 값 쓰기!
+        print('Flood is occured!')
+        Seq = SeqCounterClockwise if direction else SeqClockwise
+    else :
+        print('good!')
+        #Seq = SeqClockwise if direction else SeqCounterClockwise
 
-        StepCounter += 1
-        if StepCounter == StepCount:
-            StepCounter = 0
-        if StepCounter < 0:
-            StepCounter = StepCount
+    for pin in range(0, 4):
+        xpin = StepPins[pin]
+        if Seq[StepCounter][pin] != 0:
+            GPIO.output(xpin, True)
+        else:
+            GPIO.output(xpin, False)
 
-        time.sleep(0.01)
+    StepCounter += 1
+    if StepCounter == StepCount:
+        StepCounter = 0
+    if StepCounter < 0:
+        StepCounter = StepCount
 
-
-      
-except KeyboardInterrupt:
-    GPIO.cleanup()
+    time.sleep(0.01)
