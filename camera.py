fc152bee24b926dcb69646fb92b7517e7fabc3cc
Ol-Or <suna42378@gmail.com>
2023-05-07 21:38:21 +0900

    camera

--- camera.py
+++ camera.py
@@ -1,6 +1,4 @@
-import Rpi.GPIO as GPIO
 from picamera import PiCamera
-switch = 16
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(switch, GPIO.IN, GPIO.PUD_DOWN)
-camera = PiCamera() camera.start_preview()
+
+camera = PiCamera()
+camera.start_preview()
